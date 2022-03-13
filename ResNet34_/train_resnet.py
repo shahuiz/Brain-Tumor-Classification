cf91b78180c81c2a5f79360abec3b90eb7279faf
Zhang Tianyu <78294088+zty-7@users.noreply.github.com>
2022-03-13 13:11:56 +0800

    Update and rename train_resnet.py to ResNet34_/train_resnet.py

--- ResNet34_/train_resnet.py
+++ ResNet34_/train_resnet.py
@@ -15,43 +15,41 @@ from torch.utils.data import Dataset
 from resnet_model import resnet34
 
 def read_split_data(root: str, val_rate: float = 0.2):
-    random.seed(0)  # 保证随机结果可复现
+    random.seed(0)  
     assert os.path.exists(root), "dataset root: {} does not exist.".format(root)
 
-    # 遍历文件夹，一个文件夹对应一个类别
     tumor_class = [cla for cla in os.listdir(root) if os.path.isdir(os.path.join(root, cla))]
-    # 排序，保证顺序一致
     tumor_class.sort()
-    # 生成类别名称以及对应的数字索引
+
     class_indices = dict((k, v) for v, k in enumerate(tumor_class))
     json_str = json.dumps(dict((val, key) for key, val in class_indices.items()), indent=4)
     with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
-    train_images_path = []  # 存储训练集的所有图片路径
-    train_images_label = []  # 存储训练集图片对应索引信息
-    val_images_path = []  # 存储验证集的所有图片路径
-    val_images_label = []  # 存储验证集图片对应索引信息
-    every_class_num = []  # 存储每个类别的样本总数
-    supported = [".jpg", ".JPG", ".png", ".PNG"]  # 支持的文件后缀类型
-    # 遍历每个文件夹下的文件
+    train_images_path = []  
+    train_images_label = [] 
+    val_images_path = []  
+    val_images_label = [] 
+    every_class_num = []  
+    supported = [".jpg", ".JPG", ".png", ".PNG"]  
+   
     for cla in tumor_class:
         cla_path = os.path.join(root, cla)
-        # 遍历获取supported支持的所有文件路径
+   
         images = [os.path.join(root, cla, i) for i in os.listdir(cla_path)
                   if os.path.splitext(i)[-1] in supported]
-        # 获取该类别对应的索引
+   
         image_class = class_indices[cla]
-        # 记录该类别的样本数量
+   
         every_class_num.append(len(images))
-        # 按比例随机采样验证样本
+   
         val_path = random.sample(images, k=int(len(images) * val_rate))
 
         for img_path in images:
-            if img_path in val_path:  # 如果该路径在采样的验证集样本中则存入验证集
+            if img_path in val_path: 
                 val_images_path.append(img_path)
                 val_images_label.append(image_class)
-            else:  # 否则存入训练集
+            else: 
                 train_images_path.append(img_path)
                 train_images_label.append(image_class)
 
@@ -74,7 +72,6 @@ class MyDataSet(Dataset):
 
     def __getitem__(self, item):
         img = Image.open(self.images_path[item])
-        # RGB为彩色图片，L为灰度图片
         if img.mode != 'RGB':
             raise ValueError("image: {} isn't RGB mode.".format(self.images_path[item]))
         label = self.images_class[item]
@@ -86,7 +83,6 @@ class MyDataSet(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        # 官方实现的default_collate可以参考
         # https://github.com/pytorch/pytorch/blob/67b7e751e6b5931a9f45274653f4f653a4e6cdf6/torch/utils/data/_utils/collate.py
         images, labels = tuple(zip(*batch))
 
